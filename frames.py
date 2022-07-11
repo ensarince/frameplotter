41c8c3c88b746bdcb0fdbc12d50186a2f8d65f32
ensarince <32306029+ensarince@users.noreply.github.com>
2022-07-11 18:22:01 +0200

    Update frames.py

--- frames.py
+++ frames.py
@@ -3,12 +3,12 @@ import os
 from os import listdir
 
 outpath = './frames'
-vidcap = cv2.VideoCapture('octopus.mp4')
+vidcap = cv2.VideoCapture('output.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  #save_name = os.path.join(outpath + '.jpg')
-  cv2.imwrite("frame%d.jpg" % count, image)   # save frame as JPEG file      
+  vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*100))    # added this line 
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+  print ('Read a new frame: ', success)
+  cv2.imwrite( outpath + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+  count = count + 1
